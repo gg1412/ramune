4f1b75bf52784467abdde78d8d3cc422ccef17c9
gg1412 <78941029+gg1412@users.noreply.github.com>
2021-11-26 16:40:06 +0900

    Add files via upload

--- ramune2/history.php
+++ ramune2/history.php
@@ -12,14 +12,26 @@ $u_id = $_SESSION['user_id'];
 $historyData = getMyhistory($u_id);
 ?>
 
-<style>
+<!-- <style>
   .example {
     width:50%;
     height:50%;
   }
-</style>
 
+  .left {
+    float:left;
+  }
+</style> -->
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
 <div id="contents" class="site-width">
 <h1 class="page-title">購入済み商品</h1>
 <section id="main">
@@ -33,8 +45,7 @@ $historyData = getMyhistory($u_id);
               <img src="<?php echo showImg(sanitize($val['pic'])); ?>" alt="<?php echo sanitize($val['product_name']); ?>" class="example">
             </div>
             <div class="panel-body">
-              <p class="panel-title">
-                出品者:<?php echo sanitize($val['user_name']); ?>
+                出品者:<?php if(!empty($val['user_name'])) { echo sanitize($val['user_name']); } else { echo "noname"; } ?>
                 <br>
                 商品名:<?php echo sanitize($val['product_name']); ?>
                 <br>
@@ -46,4 +57,8 @@ $historyData = getMyhistory($u_id);
           endif;
           ?>
 </section>
+        </div>
 
+
+</body>
+</html>
